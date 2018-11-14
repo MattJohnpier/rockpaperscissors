1cc186546d9a715f4d7895fb9390c3e6cc95351f
matthew johnpier <mattjohnpier@gmail.com>
2018-11-14 23:14:28 +0000

    randomNumbers

--- js/script.js
+++ js/script.js
@@ -6,8 +6,20 @@
 /* global $ */
 
 
-
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function() {
-    $("#userChoice").text($("#input").val());
+    var userPick = $("input").val();
+    var randomNumber = Math.random();
+    var computerChoice;
+    $("#userChoice").text(userPick);
+    if (randomNumber < .333){
+        computerChoice = "rock";
+    } else if( (randomNumber > .333) && (randomNumber < .666) ){
+        computerChoice = "paper";
+    } else if (randomNumber > .666){
+        computerChoice = "scissors";
+    }
 });
+
+var computerChoice = "";
+var winner = "";
