833ec3c5edbdc8598e813fbe6ff548b296f4a5a8
matthew johnpier <mattjohnpier@gmail.com>
2018-11-28 23:25:31 +0000

    DONE

--- js/script.js
+++ js/script.js
@@ -11,7 +11,10 @@ $("#shoot").click(function() {
     var userPick = $("input").val();
     var randomNumber = Math.random();
     var computerChoice;
+    
     $("#userChoice").text(userPick);
+
+    
     if (randomNumber < .333){
         computerChoice = "rock";
     } else if( (randomNumber > .333) && (randomNumber < .666) ){
@@ -19,7 +22,29 @@ $("#shoot").click(function() {
     } else if (randomNumber > .666){
         computerChoice = "scissors";
     }
+    
+    if ((userPick === "scissors" && computerChoice === "paper")    || 
+        (userPick === "rock"     && computerChoice === "scissors") || 
+        (userPick === "paper"    && computerChoice === "rock")){
+            
+            $("#result").html("You Win!"); 
+    }else if ((userPick === "paper"    && computerChoice === "scissors") || 
+              (userPick === "scissors" && computerChoice === "rock")     || 
+              (userPick === "rock"     && computerChoice === "scissors")){
+                  $("#result").html("Computer Wins!");
+                  
+    }else if ((userPick === "scissors" && computerChoice === "scissors")  || 
+             (userPick === "rock"     && computerChoice === "rock")      || 
+             (userPick === "paper"    && computerChoice === "paper")){
+                  $("#result").html("It's a Tie!");
+    
+    }else {
+        $("#result").html("...what?");
+    }
+             
+    $("#computerChoice").text(computerChoice);
+    $("#input").val("");
+
 });
 
-var computerChoice = "";
 var winner = "";
